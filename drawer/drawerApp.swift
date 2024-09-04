eb630b325bd48d7ace129765beb5f18ab4aac796
Tnixc <tnixxc@gmail.com>
2024-09-04 17:49:44 -0400

    Initial Commit

---
+++ drawer/drawerApp.swift
@@ -0,0 +1,17 @@
+//
+//  drawerApp.swift
+//  drawer
+//
+//  Created by tnixc on 4/9/2024.
+//
+
+import SwiftUI
+
+@main
+struct drawerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
